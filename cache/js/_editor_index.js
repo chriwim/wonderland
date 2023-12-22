46ee40ee26b14bfaa5ed183c11292e8becb3836d
Christoph Wimmer <christoph.wimmer@gmail.com>
2023-12-22 20:52:38 +0100

    Initial commit

---
+++ cache/js/_editor_index.js
@@ -0,0 +1,4 @@
+import * as __wonderlandengine_components from '@wonderlandengine/components';
+_registerEditor(__wonderlandengine_components);
+import * as __Users_christoph_Documents_GitHub_wonderland_js_button_js from '/Users/christoph/Documents/GitHub/wonderland/js/button.js';
+_registerEditor(__Users_christoph_Documents_GitHub_wonderland_js_button_js);
